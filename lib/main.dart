cc8abd7da52bd31bda7e728cd40d87b91fded668
Kurt Bryant <BCArpilleda@gmail.com>
2026-05-16 21:00:57 +0800

    icon logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'splashScreen.dart';
 import 'login.dart';
 import 'dashboard.dart';
 import 'checkAccount.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'api_service.dart';
+import 'theme/app_theme.dart';
 
-final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> navigatorKey =
+GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   ApiService.setupHttpOverrides();
+
   final prefs = await SharedPreferences.getInstance();
   final isFirstOpen = prefs.getBool('isFirstOpen') ?? true;
+
   final apiService = ApiService();
   final authToken = await apiService.getAuthToken();
 
-  runApp(MyApp(
-    initialRoute: isFirstOpen
-        ? '/splash'
-        : authToken != null
-        ? '/checkAccount'
-        : '/login',
-  ));
+  runApp(
+    MyApp(
+      initialRoute: isFirstOpen
+          ? '/splash'
+          : authToken != null
+          ? '/checkAccount'
+          : '/login',
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   final String initialRoute;
-  const MyApp({required this.initialRoute, Key? key}) : super(key: key);
+
+  const MyApp({
+    required this.initialRoute,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-    });
-
     return MaterialApp(
       navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       title: 'CARE',
-      theme: ThemeData(
-        fontFamily: 'Lato-Black',
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
-      ),
+      theme: AppTheme.lightTheme,
       initialRoute: initialRoute,
       routes: {
         '/splash': (context) => const SplashScreen(),
+
         // '/login': (context) => const LoginScreen(),
         // '/checkAccount': (context) => const CheckAccountScreen(),
         // '/dashboard': (context) => const DashboardScreen(),
-        // '/shopMessages': (context) => const ShopOwnerMessageListScreen(),
       },
     );
   }
---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color white = Colors.white;
+  static const Color darkGray = Color(0xFF333333);
+  static const Color redPink = Color(0xFFD53A47);
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Lato-Black',
+
+    scaffoldBackgroundColor: white,
+    primaryColor: redPink,
+
+    colorScheme: const ColorScheme(
+      brightness: Brightness.light,
+
+      primary: redPink,
+      onPrimary: white,
+
+      secondary: darkGray,
+      onSecondary: white,
+
+      error: Colors.red,
+      onError: white,
+
+      surface: white,
+      onSurface: darkGray,
+    ),
+
+    appBarTheme: const AppBarTheme(
+      backgroundColor: redPink,
+      foregroundColor: white,
+      elevation: 0,
+      centerTitle: true,
+    ),
+
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(
+        color: darkGray,
+      ),
+      bodyMedium: TextStyle(
+        color: darkGray,
+      ),
+      titleLarge: TextStyle(
+        color: darkGray,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: redPink,
+        foregroundColor: white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    ),
+
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: white,
+
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: const BorderSide(color: darkGray),
+      ),
+
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: const BorderSide(color: darkGray),
+      ),
+
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: const BorderSide(
+          color: redPink,
+          width: 2,
+        ),
+      ),
+    ),
+  );
+}
