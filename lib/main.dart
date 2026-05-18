ef83f692a4d6d8b20544762c0213b158a3c78f76
Kurt Bryant <BCArpilleda@gmail.com>
2026-05-18 20:50:42 +0800

    signup

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'CARE',
+      title: 'CiviCall',
       theme: AppTheme.lightTheme,
       initialRoute: initialRoute,
       routes: {
